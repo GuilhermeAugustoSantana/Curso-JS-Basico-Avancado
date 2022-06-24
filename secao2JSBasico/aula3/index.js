428cdbf8d63ce1d300cdb0fdc3313014f6e96483
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 11:34:31 -0400

    Removi

--- secao2JSBasico/aula3/index.js
+++
@@ -1,5 +0,0 @@
-console.log('Guilherme "augusto"');
-console.log("Guilherme 'augusto'");
-console.log(`'Guilherme' "toma"`);
-
-console.log(12, 15.85, 'Guilherme');
--- secao2JSBasico/aula8/index.js
+++
@@ -1,9 +0,0 @@
-const nome = 'Guilherme';
-const alturaEmM = 1.75;
-const peso = 56.80;
-const idade = 18;
-
-let anoNascimento = 2022 - 18;
-let indiceMassaCorporal = peso / (alturaEmM * alturaEmM);
-
-console.log(`Meu nome é ${nome}, tenho ${idade} anos, nasci em ${anoNascimento}, tenho de peso ${peso} Kg, tenho ${alturaEmM} metros de altura e meu IMC é ${indiceMassaCorporal}.`);
