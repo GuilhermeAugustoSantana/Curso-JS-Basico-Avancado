e9ea4f8ceeee5e2e7d40bbade13e6aa15d73e030
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 10:32:31 -0400

    Aprendi mais sobre strings

---
+++ secao2JSBasico/aula9/indexx.js
@@ -0,0 +1,5 @@
+const nome = 'gui'; // string
+const num1 = 1; // number
+let nomeAluno; // undefined -> não aponta para local nenhuma na memória
+let sobrenomeAluno = null;// nulo -> não aponta para local nenhuma na memória
+const aprovado = false;// boolean = true, false (lógico)
