b3fa55f03231314dd42b1183d7c3a93692ae242f
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 11:31:24 -0400

    Aprendi a primeria difereça do let vs var e também sobre os tipos de dados primitivos

---
+++ secao2JSBasico/aula09/index.js
@@ -0,0 +1,6 @@
+//nome = 'guilherme';//não fazer isso
+//utilize o let não o var
+let nome = 'guilherme';
+//diferença do var com let, é que o var você pode redeclarar a varialvel e isso é um comportamento indesejado , já com let não pode
+
+console.log(nome);
