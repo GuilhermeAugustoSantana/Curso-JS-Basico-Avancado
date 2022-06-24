494917037e7f3c553ed1aa023f1ab2227abe52af
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 10:34:21 -0400

    Realizei um exercicio sobre strings

---
+++ secao2JSBasico/aula10/index.js
@@ -0,0 +1,16 @@
+const num1 = 10;
+const num2 = 5;
+const num3 = 2;
+//subtração
+console.log(num1 - num2);
+//adição
+console.log(num1 + num2);
+//Potenciação
+console.log(num1 ** num2);
+//Resto da divisão
+console.log(num1 % num2);
+
+//NaN - Not a number, parseInt (inteiro), parseFloat(decimais)
+const num4 = 10;
+const num5 = parseFloat('5.2');
+console.log(num4 + num5);
