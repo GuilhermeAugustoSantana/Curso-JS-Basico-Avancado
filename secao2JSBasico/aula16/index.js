991171585ae9051b28dcea6414b30e1b4239d5cd
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 11:09:43 -0400

    Aprendi o básico de arrays

---
+++ secao2JSBasico/aula16/index.js
@@ -0,0 +1,26 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+// console.log(alunos instanceof Array);
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, -2));
+// delete alunos[1];
+
+// const removido = alunos.shift();
+// const removido = alunos.pop();
+// console.log(removido)
+
+// alunos.unshift('Luiza'); Adiciona no começo
+// alunos.unshift('Fabio');
+
+// alunos.push('Luiz'); Adiciona no fim
+// alunos.push('Fabio');
+
+// alunos[alunos.length] = 'Luiza'; Adiciona no fim
+
+// alunos[0] = 'Eduardo'; Altera
+// alunos[3] = 'Luiza'; Adicionando
+// console.log(alunos[0]); 
+
+
