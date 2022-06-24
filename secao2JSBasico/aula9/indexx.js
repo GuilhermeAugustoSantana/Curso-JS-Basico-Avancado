40a1192510e84ee933b0156188fcd37420ebced6
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 10:51:56 -0400

    Aprendi a primeira diferença do let vs var

--- secao2JSBasico/aula9/indexx.js
+++ secao2JSBasico/aula9/indexx.js
@@ -3,3 +3,4 @@ const num1 = 1; // number
 let nomeAluno; // undefined -> não aponta para local nenhuma na memória
 let sobrenomeAluno = null;// nulo -> não aponta para local nenhuma na memória
 const aprovado = false;// boolean = true, false (lógico)
+
