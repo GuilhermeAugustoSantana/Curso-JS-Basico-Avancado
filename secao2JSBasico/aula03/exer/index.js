08332578011076a7dc8908b5e6e46384ce16845b
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 11:23:57 -0400

    Aprendi sobre console.log

---
+++ secao2JSBasico/aula03/exer/index.js
@@ -0,0 +1,1 @@
+console.log('MEu nome é "GUilherme". Estou aprendendo JavaScript às 10 da manhã');
---
+++ secao2JSBasico/aula03/index.js
@@ -0,0 +1,5 @@
+console.log('Guilherme "augusto"');
+console.log("Guilherme 'augusto'");
+console.log(`'Guilherme' "toma"`);
+
+console.log(12, 15.85, 'Guilherme');
