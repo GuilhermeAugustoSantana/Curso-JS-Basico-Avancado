64d071ad99559e9d1fb4857f14e215f47ae83295
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-24 11:00:47 -0400

    Aprendi mais sobre strings

---
+++ secao2JSBasico/aula13/index.js
@@ -0,0 +1,23 @@
+//               01234567
+let umaString = 'Um texto';
+
+console.log(umaString.charAt(3));
+
+console.log(umaString.concat(' em um lindo dia.'))
+
+console.log(umaString + ' em um lindo dia.');
+
+console.log(`${umaString} em um lindo dia.`);
+
+console.log(umaString.indexOf('o', 3));
+
+console.log(umaString.lastIndexOf('m', 3));
+
+console.log(umaString.length);
+
+console.log(umaString.slice(3,8));
+
+console.log(umaString.split(' '));
+
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
