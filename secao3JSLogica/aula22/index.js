c2e949b3e9ca52bdf11baf8c34c2a874caa31d79
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:45:16 -0400

    Aprindi sobre operadores lógicos

---
+++ secao3JSLogica/aula22/index.js
@@ -0,0 +1,11 @@
+/*
+operadores lógicos
+&& -> and -> todas as expressões precisam ser verdadeiras para retornar true
+|| -> or
+! -> not
+*/
+const expressaoAnd = true && true;
+const expressaoOr = true || false;
+
+console.log(expressaoOr);
+console.log(!true);
