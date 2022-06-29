76a7120c67da04b8e76cfc86e2f2331e1abf9ed0
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:59:37 -0400

    Aprendi sobre break e continue

---
+++ secao3JSLogica/aula40/index.js
@@ -0,0 +1,23 @@
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Continue - continua para próxima interação
+// Break - sai do laço
+
+for (let numero of numeros) {
+
+    if (numero === 2) continue;
+
+    console.log(numero);
+
+    if (numero === 3) break;
+}
+
+for (let i in numeros) {
+    let numero = numeros[i];
+
+    if (numero === 2) continue;
+
+    console.log(numero);
+
+    if (numero === 3) break;
+}
