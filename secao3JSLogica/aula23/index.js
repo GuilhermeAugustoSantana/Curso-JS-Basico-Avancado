67ed52ae96b292d4c717975604d813fa9ef9aca5
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:46:14 -0400

    Aprindi sobre avaliação de curto-circuito(short-circuit)

---
+++ secao3JSLogica/aula23/index.js
@@ -0,0 +1,13 @@
+/* 
+&& -> false && true -> false "o valor mesmo"
+|| -> true || false -> vai retornar "o valor verdadeiro"
+
+FALSY
+false
+0
+'' "" ``
+null / undefined
+NaN
+*/
+console.log('Guilherme' && false && 'Maria');
+console.log(0 || false || null || 'Guilherme Augusto');
