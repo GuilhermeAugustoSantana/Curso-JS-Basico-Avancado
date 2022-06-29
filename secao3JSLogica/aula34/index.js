367d7a6a6d4dc6691d94afd67186b624b53093c7
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:55:07 -0400

    Aprendi sobre For - estrutura de repetição

---
+++ secao3JSLogica/aula34/index.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 5; i++) {
+    const par = i % 2 === 0 ? 'par' : 'impar';
+    console.log(i, par);
+}
+
+const frutas = ['Maça', 'Pêra', 'Uva'];
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Indice ${i}`, frutas[i]);
+}
