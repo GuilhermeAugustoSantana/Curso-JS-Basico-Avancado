3efef5bebca29e19a97abf915fda756fb0eb06f9
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:55:42 -0400

    Realisei exercícios com for

---
+++ secao3JSLogica/aula35/assets/js/main.js
@@ -0,0 +1,30 @@
+function myEscop(){
+
+    const elementos = [
+        { tag: 'p', texto: 'Frase 1' },
+        { tag: 'div', texto: 'Frase 2' },
+        { tag: 'footer', texto: 'Frase 3' },
+        { tag: 'section', texto: 'Frase 4' },
+    ];
+
+    const container = document.querySelector('.container');
+    const div = document.createElement('div');
+
+    function setTagsDom(elements) {      
+        for (let i = 0; i < elements.length; i++){
+            let { tag, texto } = elements[i];
+            let tagCriada = document.createElement(tag);
+            let textoCriado = document.createTextNode(texto);
+
+            // tagCriada.innerText = texto;
+            tagCriada.appendChild(textoCriado);
+            div.appendChild(tagCriada);
+        }    
+    }
+
+    container.appendChild(div);
+    setTagsDom(elementos);
+
+}
+
+myEscop();
