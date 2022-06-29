4f38e022e61e994903b7b9756cf769300c4cc9aa
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-06-28 21:52:34 -0400

    Aprendi sobre atribuição via desestruturação(arrays)

---
+++ secao3JSLogica/aula32/index.js
@@ -0,0 +1,10 @@
+// const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const [um, ,tres] = numeros;
+
+// console.log(um, tres);
+
+const numeros = [[1, 2, 3], [1, 2, 3], [1, 2, 3]];
+// const [,[,,seis]] = numeros;
+const [lista1, lista2, lista3] = numeros;
+
+console.log(lista2[2]);
