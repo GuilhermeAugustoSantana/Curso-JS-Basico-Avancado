0fe7cd20197a1e1b39910033a0c23a4016b52bbf
Guilherme Santana <guilherme09augustoadams@gmail.com>
2022-07-29 09:17:31 -0400

    Revisão do básico em Arrays

---
+++ secao5JSArrays(Avancado)/aula62/index.js
@@ -0,0 +1,27 @@
+// const nome = 'Guilherme augusto adams';
+// const nomes = nome.split(' ');
+const nomes = [ 'Guilherme', 'augusto', 'adams' ]
+const nome = nomes.join(' ');
+console.log(nome);
+
+
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'João'];
+// const novo = [...nomes];
+
+// const novo = nomes.slice(1, 3);
+// console.log(novo);
+
+// nomes.push('João');
+// nomes.unshift('Joã');
+// console.log(nomes);
+
+// const removido = nomes.shift();
+
+// const removido = nomes.pop();
+// console.log(nomes, removido);
+
+// console.log(nomes.length);
+
+// novo.pop();
+// console.log(nomes);
+// console.log(novo);
